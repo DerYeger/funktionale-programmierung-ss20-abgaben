0c6df96258f9642fed3f9f508e6a47fa1fd88e43
Jan Müller <janmueller3698@gmail.com>
2020-04-28 16:14:30 +0200

    Applied feedback

--- Woche 2/aufgabeB.hs
+++ Woche 2/aufgabeB.hs
@@ -15,16 +15,16 @@ merge (x:xs) (y:ys)
     | x < y = x:merge xs (y:ys)
     | otherwise = y:merge (x:xs) ys
 
-mergesort :: (Ord a) => [a] -> [a]
-mergesort xs 
-        | (length xs) <= 1 = xs
-        | otherwise = merge (mergesort ls) (mergesort rs)
-        where
-            ls = take ((length xs) `div` 2) xs
-            rs = drop ((length xs) `div` 2) xs
+mergesort :: Ord a => [a] -> [a]
+mergesort list =
+    case list of
+        [] -> []
+        [a] -> [a]
+        otherwise -> merge (mergesort $ take half list) (mergesort $ drop half list)
+            where half = (length list) `div` 2
 
 remove_ao :: String -> String
-remove_ao input = map (\c -> if c=='a' || c=='o' then '_' else c) input
+remove_ao input = map (\char -> if (char `elem` ['a', 'o']) then '_' else char) input
 
 reversePairList :: [(a, b)] -> [(b, a)]
 reversePairList list = reverse [(b, a) | (a, b) <- list]
@@ -32,11 +32,14 @@ reversePairList list = reverse [(b, a) | (a, b) <- list]
 remove_01 :: [(Int, Int)] -> [(Int, Int)]
 remove_01 list = filter (\(first, second) -> first /= 0 && second /= 1) list
 
-removeDuplicates :: (Eq a) => [a] -> [a]
-removeDuplicates [] = []
-removeDuplicates (x:xs)
-    | x `elem` xs = removeDuplicates xs
-    | otherwise = x:(removeDuplicates xs)
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates list = 
+    let
+        helper [] acc = acc
+        helper (head:tail) acc = if (head `elem` acc)
+            then helper tail acc -- element is a duplicate so we ignore it
+            else helper tail (head:acc) -- element is new so we add it to the accumulator
+    in reverse $ helper list []
     
 mergesort_let :: (Ord a) => [a] -> [a]
 mergesort_let xs 
