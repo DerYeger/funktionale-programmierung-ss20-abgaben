fb8ec668170e99179b10ac251bf789957357c5db
Jan Müller <janmueller3698@gmail.com>
2020-05-04 12:16:45 +0200

    Assignment a

---
+++ Woche 3/aufgaben.hs
@@ -0,0 +1,9 @@
+-- Infos
+
+-- Juri Lozowoj, 35244015
+-- Jan Müller, 35011918
+
+-- Aufgabe a
+
+someFunction :: [Integer]
+someFunction = replicate 2 . product . map (*3) $ zipWith max [1, 2] [4, 5]
