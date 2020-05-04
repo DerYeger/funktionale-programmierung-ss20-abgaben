27cde905e977b6b5dccb83014304de14a0f8bf3b
Jan Müller <janmueller3698@gmail.com>
2020-05-04 12:46:06 +0200

    Assignment c

--- Woche 3/aufgaben.hs
+++ Woche 3/aufgaben.hs
@@ -16,3 +16,17 @@ myConcat = foldl (++) []
 myElem :: (Foldable t, Eq a) => a -> t a -> Bool 
 myElem x = foldl check False
     where check found y = found || x == y
+
+-- Aufgabe c
+
+-- countElem 1 [1, 2, 3, 1, 4, 1, 5, 6, 1, 7]
+
+countElem :: (Foldable t, Eq a) => a -> t a -> Integer
+countElem x = foldr count 0
+    where count y acc = acc + if (x == y) then 1 else 0
+
+-- myId [1, 3, 5, 7, 9, 8, 6, 4, 2, 0]
+
+myId :: [a] -> [a]
+myId = foldr reconstruct []
+    where reconstruct y acc = y:acc
