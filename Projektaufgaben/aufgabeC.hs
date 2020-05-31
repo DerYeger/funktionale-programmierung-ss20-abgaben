8b7ddd8285407ff524adf5e517075e0c8594b3be
Jan Müller <janmueller3698@gmail.com>
2020-05-31 17:08:12 +0200

    Changes

--- Projektaufgaben/aufgabeC.hs
+++ Projektaufgaben/aufgabeC.hs
@@ -5,35 +5,47 @@ import System.Random (randomRIO)
 data Strategy = Bad | Nice
     deriving (Eq)
 
-data Player = A | B
-    deriving (Show)
+data Player = Player {name::String, location::Int, target::Int}
+instance Show Player where
+    show (Player n l t) = n ++ " (" ++ show l ++ ", " ++ show t ++ ")"
 
-type Pos = (Int, Player) 
-type State = (Pos, Pos)
+setLocation :: Player -> Int -> Player
+setLocation (Player n l t) nl = Player n nl t
+
+data State = InProgress {currentPlayer::Player, otherPlayer::Player} | GameOver {winner::Player}
+instance Show State where
+    show (InProgress cp op) = "Current Player: " ++ show cp ++ "\nWaiting Player: " ++ show op ++ "\n"
 
 start :: State
-start = ((1, A), (8, B))
+start = InProgress (Player "A" 1 1) (Player "B" 8 8)
 
-nextPos strategy ((al, ap), (pl , pp)) d
-    | trgt /= pl = ((pl, pp), (trgt, ap))
-    | strategy == Bad = ((0, pp), (trgt, ap))
-    | otherwise = ((pl, pp), (trgt - 1, ap))
+nextPos _ s@(GameOver _) _ = s
+nextPos strategy (InProgress cp@(Player _ cl ct) op@(Player _ ol ot)) d
+    | ct /= ol = InProgress op (setLocation cp nl)
+    | strategy == Bad = GameOver cp
+    | otherwise = InProgress op (setLocation cp (onField nl - 1))
         where 
-            trgt = next (al + d)
+            nl = onField (cl + d)
 
 turn :: IO State -> IO State
-turn state = do
+turn sio = do
     d <- rollDice
-    os <- state
-    putStr $ show (snd . fst $ os) ++ " rolled " ++ show d ++ "\n"
+    os <- sio
+    let cpName = name . currentPlayer $ os
+    putStr $ cpName ++ " rolled " ++ show d ++ "\n"
     let ns = nextPos Nice os d
-    putStr $ show ns ++ "\n\n"
-    pure ns
+    putStr $ show ns ++ "\n"
+    return ns
 
 firstTurn = turn . pure $ start
 
 rollDice :: IO Int
 rollDice = randomRIO (1, 6)
 
-next :: Int -> Int
-next = flip mod 24
+onField :: Int -> Int
+onField = flip mod 24
+
+
+-- ludoInteractive :: IO()
+-- ludoInteractive = do
+ 
