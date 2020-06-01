7114ac92c1dcdeb34f0a6616925d998967714fcd
Jan Müller <janmueller3698@gmail.com>
2020-06-01 11:53:40 +0200

    WIP

--- Projektaufgaben/aufgabeC.hs
+++ Projektaufgaben/aufgabeC.hs
@@ -1,6 +1,7 @@
 module AufgabeC where
 
 import System.Random (randomRIO)
+import Data.Maybe (fromJust)
 
 data Strategy = Bad | Nice
     deriving (Eq, Show)
@@ -23,7 +24,7 @@ move (Player n o l r s) nl = Player n o nl (r - rd) s
 data State = InProgress {currentPlayer::Player, otherPlayer::Player} | GameOver {winner::Player}
 instance Show State where
     show (InProgress cp op) = "Current Player: " ++ show cp ++ "\nWaiting Player: " ++ show op ++ "\n"
-    show (GameOver w) = show (name w) ++ " has won the game.\n"
+    show (GameOver w) = "Player "++ name w ++ " has won the game.\n"
 
 start :: State
 start = InProgress (Player "A" 1 1 fieldSize (Just Bad)) (Player "B" 8 8 fieldSize (Just Nice))
@@ -36,11 +37,13 @@ nextPos :: State -> IO State
 nextPos s@(GameOver _) = return s
 nextPos s@(InProgress cp@(Player _ _ cl _ _) op@(Player _ _ ol _ _)) = do
     d <- randomRIO (1, 6)
+    printState s d (isInteractiveGame s)
     let nl = onField (cl + d)
     if nl == ol
         then applyStrat s <$> getStrat cp
         else return $ InProgress op (move cp nl) -- just move
 
+toOrigin :: Player -> Player
 toOrigin (Player n o l r s) = Player n o o fieldSize s
 
 checkWinner :: State -> State
@@ -63,24 +66,39 @@ playRound sio = do
         (GameOver _) -> pure ns
         (InProgress _ _) -> playRound (pure ns)
 
-automatedRounds :: Int -> IO (Int, Int)
-automatedRounds rc =
+automatedRounds :: Int -> State -> IO (Int, Int)
+automatedRounds rc start =
     if rc <= 0 
         then return(0,0) 
         else do
-            (aw, bw) <- automatedRounds $ rc - 1
+            (aw, bw) <- automatedRounds (rc - 1) start
             w <- winner <$> playRound (pure start)
-            if name w == "A" then return (aw + 1, bw) else return (aw, bw + 1)
+            if name w == name (currentPlayer start) then return (aw + 1, bw) else return (aw, bw + 1)
 
 ludoStatistic :: Int -> IO ()
 ludoStatistic rc = do
-    (aw, bw) <- automatedRounds rc
-    putStr $ "A has won " ++ show aw ++ " round(s) using the " ++ show Bad ++ " strategy.\n"
-        ++ "B has won " ++ show bw ++ " round(s) using the " ++ show Nice ++ " strategy.\n"
+    let playerA = Player "A" 1 1 fieldSize (Just Bad)
+    let playerB = Player "B" 8 8 fieldSize (Just Nice)
+    (aw, bw) <- automatedRounds rc (InProgress playerA playerB)
+    putStr $ "A has won " ++ show aw ++ " round(s) using the " ++ show (fromJust (strat playerA)) ++ " strategy.\n"
+        ++ "B has won " ++ show bw ++ " round(s) using the " ++ show (fromJust (strat playerB)) ++ " strategy.\n"
 
 getStrat :: Player -> IO Strategy
-getStrat (Player _ _ _ _ (Just s)) = return s
+getStrat (Player _ _ _ _ (Just strat)) = return strat
 getStrat _ = do
-    putStr "Select your strategy (Bad, Nice)\n"
-    s <- getLine
-    if s == "Bad" then return Bad else return Nice
+    putStr "Select your strategy (Bad == Bad, Otherwise == Nice)\n"
+    strat <- getLine
+    if strat == "Bad" then return Bad else return Nice
+
+isInteractiveGame :: State -> Bool
+isInteractiveGame (InProgress cp op) = isHumanPlayer cp || isHumanPlayer op
+    where 
+        isHumanPlayer (Player _ _ _ _ Nothing) =  True
+        isHumanPlayer _ = False
+
+ludoInteractive :: IO State
+ludoInteractive = playRound (pure $ InProgress (Player "A" 1 1 fieldSize Nothing) (Player "B" 8 8 fieldSize (Just Bad)))
+
+printState :: State -> Int -> Bool -> IO ()
+printState s@(InProgress cp _) d True = putStr $ show s ++ name cp ++ " has rolled a " ++ show d ++ "\n\n"
+printState _ _ _ = return ()
