3ecf41ba0107cfde1e01e8eb328641f70ac6f1c3
Jan Müller <janmueller3698@gmail.com>
2020-06-02 12:09:44 +0200

    WIP

--- Projektaufgaben/paths.hs
+++ Projektaufgaben/paths.hs
@@ -1,4 +1,4 @@
-module PathsShort where
+module Paths where
 
 import Data.List (foldl')
 
@@ -19,12 +19,12 @@ get arr y x = (arr !! y) !! x
 buildArray :: Int -> Int -> Int -> Int -> Int -> Array
 buildArray n m z1 z2 z3 = let (_, _, rs) = foldl nextRow (z1, z2, []) [0..n-1] in rs
     where nextRow (z1, z2, rs) i = 
-            let (nz1, nz2, r) = buildRow m i z1 z2 z3
+            let (nz1, nz2, r) = buildRow m i (z1, z2, z3) 
             in (nz1, nz2, rs ++ [r]) 
 
 -- buildRow 3 0 (1,2,3)
-buildRow :: Int -> Int -> Int -> Int -> Int -> (Int, Int, [Int])
-buildRow m i z1 z2 z3 = foldl' next (z1, z2, []) [0..m-1]
+buildRow :: Int -> Int -> (Int, Int, Int) -> (Int, Int, [Int])
+buildRow m i (z1, z2, z3) = foldl' next (z1, z2, []) [0..m-1]
     where next (cz1, cz2, xs) j = 
             let 
                 x = ((cz1 * i + cz2 * j) `mod` 9) + 1
