c8270d33147a9f9d29ed3bbf002839bd81a448ca
Jan Müller <janmueller3698@gmail.com>
2020-06-07 17:03:08 +0200

    WIP

--- Projektaufgaben/groups.hs
+++ Projektaufgaben/groups.hs
@@ -4,11 +4,13 @@ import Data.List.Split (chunksOf)
 import Data.List (foldl')
 import Data.Maybe (isJust, fromJust)
 
-data Person = Person{name::String, wishes::[String]} deriving (Eq)
+data Person = Person {name::String, wishes::[String]}
 instance Show Person where
     show (Person n _) = show n
 
 type Group = [Person]
+data Solution = Solution {groups::[Group], score::Int}
+    deriving (Show)
 
 wishScores :: [Int]
 wishScores = [10, 5, 1]
@@ -16,8 +18,8 @@ wishScores = [10, 5, 1]
 asPerson :: [String] -> Person
 asPerson (n:xs) = Person n $ take 3 xs
 
-asGroups :: Group -> [Group]
-asGroups = foldr partition [[], [], []]
+partitionGroups :: Group -> [Group]
+partitionGroups = foldr partition [[], [], []]
     where partition x [xs, ys, zs] = [ys, zs, x:xs]
 
 totalScore :: [Group] -> Int
@@ -27,14 +29,23 @@ totalScore = foldl' (\acc g -> acc + groupScore g) 0
                 personScore acc (Person _ ws) = foldl' (+) acc $ zipWith checkWish ws wishScores
                 checkWish w s = if foldr (\x acc -> acc || (name x == w)) False g then s else 0
 
-optimizeGroups :: String -> IO ()
-optimizeGroups fileName = do
-    fileLines <- lines <$> readFile fileName
-    let persons = map (asPerson . words) fileLines
-    let groups = asGroups persons
-    print persons
-    print groups
-    print $ totalScore groups
+asSolution :: [Group] -> Solution
+asSolution gs = Solution gs $ totalScore gs
+
+-- TODO
+getNeighbours :: Solution -> [Solution]
+getNeighbours = pure
+
+improve :: Solution -> IO Solution
+improve s = do
+    print s
+    let ns = getNeighbours s
+    let best = foldl' (\b n -> if score n > score b then n else b) (head ns) (tail ns) -- This won't actually cause problems. Neighbours should never be empty.
+    if score best <= score s then return s else improve best
 
 main :: IO ()
-main = optimizeGroups "wishes.txt"
+main = do 
+    ls <- lines <$> readFile "wishes.txt"
+    let gs = partitionGroups $ map (asPerson . words) ls
+    improve $ asSolution gs
+    return ()
