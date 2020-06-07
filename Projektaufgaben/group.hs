8f9da02c544ddb378ae48f5ed7efe4ad0f58d05b
Jan Müller <janmueller3698@gmail.com>
2020-06-07 15:55:09 +0200

    Score calculation

--- Projektaufgaben/group.hs
+++ Projektaufgaben/group.hs
@@ -1,4 +1,5 @@
 import Data.List.Split (chunksOf)
+import Data.List (foldl')
 
 data Person = Person{name::String, first::Maybe String, second::Maybe String, third::Maybe String} deriving (Eq)
 instance Show Person where
@@ -20,10 +21,28 @@ myRead name = do
     let groups = asGroups persons
     print persons
     print groups
+    print $ getScore $ groups !! 0
+    print $ getScore $ groups !! 1
+    print $ getScore $ groups !! 2
+    print $ getTotalScore groups
 
 asGroups :: Group -> [Group]
 asGroups = foldr partition [[], [], []]
     where partition x [xs, ys, zs] = [ys, zs, x:xs]
+
+getScore :: Group -> Int
+getScore group = foldl' calc 0 group
+    where 
+        containsWish n = foldl' (\acc x -> (name x == n) || acc) False group
+        calc :: Int -> Person -> Int
+        calc acc (Person _ f s t) = acc + checkWish f 10 + checkWish s 5 + checkWish t 1
+            where 
+                checkWish wish score = case wish of
+                    Nothing -> 0
+                    Just n -> if containsWish n then score else 0
+
+getTotalScore :: [Group] -> Int
+getTotalScore = foldl' (\acc g -> acc + getScore g) 0
 
 main :: IO ()
 main = myRead "wish.txt"
