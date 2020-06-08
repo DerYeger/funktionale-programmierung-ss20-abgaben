1cfd7792f919005fe0c2c9721f144fed29e7c926
Jan Müller <janmueller3698@gmail.com>
2020-06-08 12:57:33 +0200

    WIP

--- Projektaufgaben/groups.hs
+++ Projektaufgaben/groups.hs
@@ -43,11 +43,8 @@ getMoveNeighbours s@(Solution gs@[xs, ys, zs] _)
     | length (head gs) == length (gs !! 2) = pure gs -- 3 large equals groups
     | length (gs !! 1) > length (head gs) = addCombs xs ys zs ++ addCombs xs zs ys-- 2 large and 1 small group
     | otherwise = addCombs xs zs ys ++ addCombs ys zs xs -- 1 large and 2 small groups
-
-addCombs :: Group -> Group -> Group -> [[Group]]
-addCombs t s n = foldl' (\acc (p, ps) -> [ps, n, p:t]:acc) [] (removeCombs s)
-    where removeCombs gs = foldl' rem [] gs
-            where rem acc p = (p, delete p gs):acc
+    where addCombs t s n = foldl' (\acc (p, ps) -> [ps, n, p:t]:acc) [] (removeCombs s)
+            where removeCombs gs = foldl' (\acc p -> (p, delete p gs):acc) [] gs
 
 localSearch :: Solution -> IO Solution
 localSearch s = do
